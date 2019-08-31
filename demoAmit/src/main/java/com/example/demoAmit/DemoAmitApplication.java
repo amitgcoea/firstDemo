6f13962c9acfe85abf0166d3af3fbde4e689afcc
amitgcoea <amitgcoea@gmail.com>
2019-08-31 15:48:06 +0530

    saving again

---
+++ demoAmit/src/main/java/com/example/demoAmit/DemoAmitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAmitApplication.class, args);
+	}
+
+}
